04ad4a2bd5c6c067d139ac7f23a03d2ede9a1bb3
Siya-12 <aggarwalsiya07@gmail.com>
2025-12-28 01:03:21 +0530

    title

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,38 +1,38 @@
 export default function TopBar() {
   return (
-    <div className="bg-red-200 text-red-800 text-sm">
+    <div className="bg-primary text-white text-sm">
       <div className="max-w-7xl mx-auto flex justify-between px-4 py-2">
         <div className="flex gap-6">
           <a
   href="mailto:saharaortho68@gmail.com"
-  className="flex items-center gap-2 hover:underline text-red-800"
+  className="flex items-center gap-2 hover:opacity-80 transition text-white"
 >
   ✉ saharaortho68@gmail.com
 </a>
 
 <a
   href="tel:+918700281458" target="_blank"
-  className="flex items-center gap-2 hover:opacity-80 transition text-red-800"
+  className="flex items-center gap-2 hover:opacity-80 transition text-white"
 >
   📞 +91 8700281458
 </a>
 
         </div>
-       <div className="flex gap-4 text-xl text-red-800">
+       <div className="flex gap-4 text-xl text-white">
   <a href="https://wa.me/918700281458" target="_blank" className="hover:opacity-80 transition">
-    <i className="bi bi-whatsapp text-red-800"></i>
+    <i className="bi bi-whatsapp text-white"></i>
   </a>
 
   <a href="https://in.linkedin.com/in/sahara-ortho-65a6a9266" target="_blank" className="hover:opacity-80 transition">
-    <i className="bi bi-linkedin text-red-800"></i>
+    <i className="bi bi-linkedin text-white"></i>
   </a>
 
   <a href="https://www.instagram.com/sahara_ortho?igsh=bm84c3B1eTB6d3kw" target="_blank" className="hover:opacity-80 transition">
-    <i className="bi bi-instagram text-red-800"></i>
+    <i className="bi bi-instagram text-white"></i>
   </a>
 
   <a href="https://www.facebook.com/share/1AWdeaWFAX/" target="_blank" className="hover:opacity-80 transition">
-    <i className="bi bi-facebook text-red-800"></i>
+    <i className="bi bi-facebook text-white"></i>
   </a>
 </div>
 
